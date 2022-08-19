ddb08282a16baab4c88d43bf8bd467ee8235269a
DaisukeMinagawa <iamworkingwell@gmail.com>
2022-08-20 07:34:41 +0900

    Initial commit

--- src/MapAndList.jsx
+++ src/MapAndList.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 const MapAndList = () => {
     const numbers = [2, 4, 6, 8];
 
-
     const items = numbers.map((item) => <li key={item.toString()}>{item}</li>);
-
 
     return (
         <ul>{items}</ul>
